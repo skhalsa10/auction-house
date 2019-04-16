f9687d9d7d02cf34c16e6e1b89baf569a886ed2e
mllouie <mllouie@unm.edu>
2019-04-16 10:12:31 -0600

    "Add method names"

--- src/Auction/Agent.java
+++ src/Auction/Agent.java
@@ -1,4 +1,24 @@
 package Auction;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class Agent {
+    private String name;
+    private int initialBalance;
+    private ConcurrentLinkedQueue messages;
+
+    private void openBankAccount(){}
+
+    private void chooseAuctionHouse() {}
+
+    private void connectToAuctionHouse (){
+
+    }
+
+    private void bidOnItem() {}
+
+    private void transferFunds(){}
+
+    public static void main() {}
 }
