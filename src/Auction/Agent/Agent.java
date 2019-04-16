e606e7acb14f10fde3e28ac20b3cabdba4e5cd17
mllouie <mllouie@unm.edu>
2019-04-16 11:20:34 -0600

    "Update fields and methods in agent"

--- src/Auction/Agent/Agent.java
+++ src/Auction/Agent/Agent.java
@@ -1,24 +1,40 @@
 package Auction.Agent;
 
+import Auction.Item;
+
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class Agent {
     private String name;
-    private int initialBalance;
+    private int balance;
     private ConcurrentLinkedQueue messages;
 
-    private void openBankAccount(){}
+    public Agent(String name, int initialBalance) {
+        this.name = name;
+        this.balance = initialBalance;
+    }
+    private void openBankAccount(){
+        // create message with name and initial balance
+    }
 
-    private void chooseAuctionHouse() {}
+    private void chooseAuctionHouse() {
+        // create message with host and port info
+    }
 
-    private void connectToAuctionHouse (){
+    private void connectToAuctionHouse(String auctionHost, int auctionPort){
 
     }
 
-    private void bidOnItem() {}
+    private void refreshBalance() {
+        // create message to get balance from bank
+    }
 
-    private void transferFunds(){}
+    private void bidOnItem(int amount, Item bidItem) {
+
+    }
+
+    private void transferFunds(int amount, int houseAccountNum){}
 
     public static void main() {}
 }
