58f18f46bc7924c41df44bdabebb9de9fad71ce7
mllouie <mllouie@unm.edu>
2019-04-28 19:06:43 -0600

    "add agent id to available funds message"

--- src/Auction/Messages/MAvailableFunds.java
+++ src/Auction/Messages/MAvailableFunds.java
@@ -1,14 +1,19 @@
 package Auction.Messages;
 
 public class MAvailableFunds extends Message {
-
+    private int agentId;
     private final int availableFunds;
 
-    public MAvailableFunds(int availableFunds){
+    public MAvailableFunds(int agentId, int availableFunds){
+        this.agentId = agentId;
         this.availableFunds = availableFunds;
     }
 
     public int getAvailableFunds() {
         return availableFunds;
+    }
+
+    public int getAgentId() {
+        return agentId;
     }
 }
