ed1ab09d704ad0839ef2cae0aa77aae016d611ef
mllouie <mllouie@unm.edu>
2019-04-29 18:04:28 -0600

    "Fixed MAuctionHouse message by making a copy of auction houses"

--- src/Auction/Messages/MAuctionHouses.java
+++ src/Auction/Messages/MAuctionHouses.java
@@ -5,7 +5,10 @@ import java.util.ArrayList;
 public class MAuctionHouses extends Message {
     ArrayList<MHouseServerInfo> houses;
     public MAuctionHouses(ArrayList<MHouseServerInfo> houses) {
-        this.houses = houses;
+        this.houses = new ArrayList<>();
+        for(MHouseServerInfo h: houses) {
+            this.houses.add(h);
+        }
     }
 
     public ArrayList<MHouseServerInfo> getHouses() {
